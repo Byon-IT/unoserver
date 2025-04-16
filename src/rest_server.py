d1e722e5ba1a6eb87bcbc5a7b33056bb7bee3e94
daniel <daniel@daniel.com>
2025-04-16 16:35:41 +0300

    Stress testing & tuning

--- src/rest_server.py
+++ src/rest_server.py
@@ -16,7 +16,7 @@ logger.setLevel(logging.DEBUG)
 LISTEN_INTERFACE = os.environ.get('LISTEN_INTERFACE', '0.0.0.0')
 LISTEN_PORT = int(os.environ.get('LISTEN_PORT', '5000'))
 CONVERSION_TIMEOUT = int(os.environ.get('CONVERSION_TIMEOUT', '30'))
-MEMORY_USAGE_RATIO_LIMIT = float(os.environ.get('MEMORY_USAGE_RATIO_LIMIT', '5.0'))
+MEMORY_USAGE_RATIO_LIMIT = float(os.environ.get('MEMORY_USAGE_RATIO_LIMIT', '8.0'))
 
 
 def main():
--- tests/client.py
+++ tests/client.py
@@ -1,25 +1,42 @@
 import base64
+import os
+
 import requests
+import random
 from pathlib import Path
 
 
-test_file = Path().joinpath("documents", "hebrew_word_document.docx")
-content = test_file.read_bytes()
-b64_content = base64.b64encode(content)
-
-data = {
-    "filename": "hebrew_word_document.docx",
-    "file-content": b64_content.decode("utf-8"),
-}
+test_dir = Path().joinpath("documents")
 
 
-response = requests.post("http://127.0.0.1:5000/convert-to-pdf", json=data)
-if response.status_code == 200:
-    json_data = response.json()
-    pdf_content = json_data['pdfcontent']
-    with open(Path().joinpath("documents", "converted_document.pdf"), "wb") as f:
-        f.write(base64.b64decode(pdf_content))
-else:
-    print(f"Server returned error {response.status_code}")
+def get_file_data():
+    filename = random.choice(["demo.docx",
+                              "file-sample_1MB.docx",
+                              "hebrew_word_document.docx",
+                              "sample-files.com-basic-text.docx",
+                              "sample1.docx",
+                              "sample3.docx",
+                              "sample4.docx",])
+    test_file = test_dir.joinpath(filename)
+    content = test_file.read_bytes()
+    b64_content = base64.b64encode(content)
+
+    data = {
+        "filename": "hebrew_word_document.docx",
+        "file-content": b64_content.decode("utf-8"),
+    }
+    return data
+
+
+while True:
+    data = get_file_data()
+    response = requests.post("http://127.0.0.1:5000/convert-to-pdf", json=data)
+    if response.status_code == 200:
+        json_data = response.json()
+        pdf_content = json_data['pdfcontent']
+        with open(Path().joinpath("documents", "converted_document.pdf"), "wb") as f:
+            f.write(base64.b64decode(pdf_content))
+    else:
+        print(f"Server returned error {response.status_code}")
 
 
